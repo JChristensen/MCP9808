d4998fe80fdc49ab7709ff542918e607e5048c9e
JChristensen <jack.christensen@outlook.com>
2015-04-26 18:20:41 -0400

    Return I2C status from read() and write().

--- MCP9808.cpp
+++ MCP9808.cpp
@@ -1,104 +1,91 @@
-/*-----------------------------------------------------------------------------*
- * Arduino Library for Microchip MCP9800/1/2/3 2-Wire High-Accuracy            *
- * Temperature Sensors                                                         *
- *                                                                             *
- * A lightweight implementation that exposes all functionality of the MCP980X  *
- * sensors. Temperatures are dealt with in the integer domain to avoid         *
- * the code size and runtime overhead associated with floating-point. Still,   *
- * it is easy enough to perform the necessary conversions should the user      *
- * wish to work in floating-point format.                                      *
- *                                                                             *
- * Temperatures read from the device's registers are returned as °C*16.        *
- * (If the device resolution is set to less than 12 bits, the corresponding    *
- * lower order bits are simply returned as zero.) Temperatures can             *
- * alternately be read as °F*10.                                               *
- *                                                                             *
- * When writing the Hysteresis and Limit-Set registers, the value must be      *
- * given as °C*2, which corresponds to the internal representation in these    *
- * registers.                                                                  *
- *                                                                             *
- * Bit masks for the control register are provided in the MCP980X.h file.      *
- *                                                                             *
- * Jack Christensen 21Mar2014 v1.0                                             *
- *                                                                             *
- * "Arduino library for Microchip MCP9800/1/2/3" by Jack Christensen           *
- * is licensed under CC BY-SA 4.0,                                             *
- * http://creativecommons.org/licenses/by-sa/4.0/                              *
- *-----------------------------------------------------------------------------*/
+//To do: Change write() to write one register at a time.
 
-#include <MCP980X.h>
+#include <MCP9808.h>
+#include <Streaming.h>    //http://arduiniana.org/libraries/streaming/
 
 //instantiate a temperature sensor object given the least three significant
 //bits (A2:0) of its I2C address (0-7)
-MCP980X::MCP980X(uint8_t LS_ADDR_BITS)
+MCP9808::MCP9808(uint8_t LS_ADDR_BITS)
 {
-    _devAddr = MCP980X_BASE_ADDR + (LS_ADDR_BITS & 7);
+    _devAddr = MCP9808_BASE_ADDR | (LS_ADDR_BITS & 7);
 }
 
-void MCP980X::begin(void)
+//initialize
+void MCP9808::begin(twiClockFreq_t twiFreq)
 {
     Wire.begin();
+    TWBR = ( (F_CPU / twiFreq) - 16) / 2;
+    read();
 }
 
-//read one of the sensor's three temperature registers.
-//returns the temperature as an integer which is °C times 16.
-int MCP980X::readTempC16(MCP980X_REGS_t reg)
+//read all registers from the sensor.
+//device does not support sequential access, so must read one register at a time :-|
+//returns the I2C status (zero if successful). 
+uint8_t MCP9808::read(void)
 {
-    Wire.beginTransmission(_devAddr);
-    Wire.write(reg);
-    Wire.endTransmission();
-    
-    Wire.requestFrom(_devAddr, (uint8_t)2);
-    int8_t msb = Wire.read();
-    uint8_t lsb = Wire.read();
-    return (msb << 4) + (lsb >> 4);
-}
+    uint8_t regs[SIZEOF_REGS];      //temporary storage for register data
+    uint8_t* rp = regs;
+    uint8_t e = 0;
 
-//read one of the sensor's three temperature registers.
-//returns the temperature as an integer which is °F times 10.
-int MCP980X::readTempF10(MCP980X_REGS_t reg)
-{
-    long tF160 = (long)readTempC16(reg) * 18L;
-    int tF10 = tF160 / 16;
-    if ( (tF160 & 15) >= 8) ++tF10;    //round up to the next tenth if needed
-//    if ( tF160 % 16 >= 8) ++tF10;    //(tF160 & 15) should be more efficient
-    tF10 += 320;                    //add in the offset (*10)
-    return tF10;
-}
-
-void MCP980X::writeTempC2(MCP980X_REGS_t reg, int value)
-{
-    union intByte_t {
-        int i;
-        byte b[2];
-    } t;
-    
-    if (reg > AMBIENT) {   //ambient temp reg is read-only
-        t.i = value << 7;
+    //first seven registers are two bytes each
+    for (uint8_t r = CONFIG_REG; r < N_REGS; ++r)
+    {
         Wire.beginTransmission(_devAddr);
-        Wire.write(reg);
-        Wire.write(t.b[1]);
-        Wire.write(t.b[0]);
-        Wire.endTransmission();
+        Wire.write(r);
+        if ( (e = Wire.endTransmission()) != 0 ) return e;
+        Wire.requestFrom(_devAddr, (uint8_t)2);
+        *rp++ = Wire.read();
+        *rp++ = Wire.read();
     }
-}
 
-//read the sensor's configuration register
-uint8_t MCP980X::readConfig(void)
-{
+    //last register is one byte
     Wire.beginTransmission(_devAddr);
-    Wire.write(CONFIG_REG);
+    Wire.write(RESOLUTION_REG);
+    if ( (e = Wire.endTransmission()) != 0 ) return e;
     Wire.endTransmission();
-    
     Wire.requestFrom(_devAddr, (uint8_t)1);
-    return Wire.read();
+    *rp = Wire.read();
+    
+    //assign to class members
+    config = ( regs[0] << 8 ) + regs[1];
+    tUpper    = ( ( regs[2] << 11 ) + ( regs[3] << 3 ) ) >> 5;
+    tLower    = ( ( regs[4] << 11 ) + ( regs[5] << 3 ) ) >> 5;
+    tCritical = ( ( regs[6] << 11 ) + ( regs[7] << 3 ) ) >> 5;
+    tAmbient  = ( ( regs[8] << 11 ) + ( regs[9] << 3 ) ) >> 3;
+    alertCritical = regs[8] & ALT_CRIT;
+    alertUpper    = regs[8] & ALT_UPPER;
+    alertLower    = regs[8] & ALT_LOWER;
+    mfrID = ( regs[10] << 8 ) + regs[11];
+    deviceID = regs[12];
+    deviceRev = regs[13];
+    resolution = regs[14];
+
+    return e;
 }
 
-//read the sensor's configuration register
-void MCP980X::writeConfig(uint8_t value)
+//write config, temperature and resolution registers (others are read-only)
+//device does not support sequential access, so must write one register at a time :-|
+//returns the I2C status (zero if successful). 
+uint8_t MCP9808::write(void)
 {
+    uint8_t e = 0;
+
+    //config and temperature registers
     Wire.beginTransmission(_devAddr);
-    Wire.write(CONFIG_REG);
-    Wire.write(value);
-    Wire.endTransmission();
+    Wire.write( CONFIG_REG );
+    Wire.write( config >> 8 );
+    Wire.write( config & 0xFF );
+    Wire.write( tUpper >> 11 );
+    Wire.write( (tUpper << 2) & 0xFF );
+    Wire.write( tLower >> 11 );
+    Wire.write( (tLower << 2) & 0xFF );
+    Wire.write( tCritical >> 11 );
+    Wire.write( (tCritical << 2) & 0xFF );
+    if ( (e = Wire.endTransmission()) != 0 ) return e;
+    
+    //resolution register
+    Wire.beginTransmission(_devAddr);
+    Wire.write( RESOLUTION_REG );
+    Wire.write( resolution );
+    return Wire.endTransmission();
 }
--- MCP9808.h
+++ MCP9808.h
@@ -1,9 +1,9 @@
 /*-----------------------------------------------------------------------------*
- * Arduino Library for Microchip MCP9800/1/2/3 2-Wire High-Accuracy            *
- * Temperature Sensors                                                         *
+ * Arduino Library for Microchip MCP9808 Maximum Accuracy Digital              *
+ * Temperature Sensor                                                          *
  *                                                                             *
- * A lightweight implementation that exposes all functionality of the MCP980X  *
- * sensors. Temperatures are dealt with in the integer domain to avoid         *
+ * A lightweight implementation that exposes all functionality of the MCP9808  *
+ * sensor.  Temperatures are handled in the integer domain to avoid            *
  * the code size and runtime overhead associated with floating-point. Still,   *
  * it is easy enough to perform the necessary conversions should the user      *
  * wish to work in floating-point format.                                      *
@@ -17,53 +17,90 @@
  * given as °C*2, which corresponds to the internal representation in these    *
  * registers.                                                                  *
  *                                                                             *
- * Bit masks for the control register are provided in the MCP980X.h file.      *
+ * Bit masks for the control register are provided in the MCP9808.h file.      *
  *                                                                             *
- * Jack Christensen 21Mar2014 v1.0                                             *
+ * Jack Christensen 15Apr2015 v1.0                                             *
  *                                                                             *
- * "Arduino library for Microchip MCP9800/1/2/3" by Jack Christensen           *
+ * "Arduino library for Microchip MCP9808" by Jack Christensen                 *
  * is licensed under CC BY-SA 4.0,                                             *
  * http://creativecommons.org/licenses/by-sa/4.0/                              *
  *-----------------------------------------------------------------------------*/
 
-#ifndef _MCP980X_H
-#define _MCP980X_H
+#ifndef _MCP9808_H
+#define _MCP9808_H
 
 #include <Arduino.h>
 #include <Wire.h>
 
-const int MCP980X_BASE_ADDR = 0x18;
+const int MCP9808_BASE_ADDR(0x18);
 
 //register addresses
-enum MCP980X_REGS_t { AMBIENT = 0, HYSTERESIS = 2, LIMITSET = 3 };
-const uint8_t CONFIG_REG = 1;
+const uint8_t     CONFIG_REG(0x01);
+const uint8_t     tUPPER_REG(0x02);
+const uint8_t     tLOWER_REG(0x03);
+const uint8_t  tCRITICAL_REG(0x04);
+const uint8_t   tAMBIENT_REG(0x05);
+const uint8_t     MFR_ID_REG(0x06);
+const uint8_t     DVC_ID_REG(0x07);
+const uint8_t RESOLUTION_REG(0x08);
+
+const uint8_t N_REGS(8);        //eight registers
+const uint8_t SIZEOF_REGS(15);  //the eight registers comprise 15 bytes,
+                                //two bytes each except RESOLUTION_REG is one byte
 
 //bit definitions for config register
-const uint8_t ONE_SHOT = 0x80;
-const uint8_t ADC_RES_9BITS = 0x00;
-const uint8_t ADC_RES_10BITS = 0x20;
-const uint8_t ADC_RES_11BITS = 0x40;
-const uint8_t ADC_RES_12BITS = 0x60;
-const uint8_t FAULT_QUEUE_1 = 0x00;
-const uint8_t FAULT_QUEUE_2 = 0x08;
-const uint8_t FAULT_QUEUE_4 = 0x10;
-const uint8_t FAULT_QUEUE_6 = 0x18;
-const uint8_t ALERT_POLARITY_HIGH = 0x04;
-const uint8_t INTERRUPT_MODE = 0x02;
-const uint8_t SHUTDOWN = 0x01;
+const uint16_t HYSTERESIS(0x0600);
+const uint16_t       SHDN(0x0100);
+const uint16_t    CRT_LOC(0x0080);
+const uint16_t    WIN_LOC(0x0040);
+const uint16_t    INT_CLR(0x0020);
+const uint16_t   ALT_STAT(0x0010);
+const uint16_t    ALT_CNT(0x0080);
+const uint16_t    ALT_SEL(0x0040);
+const uint16_t    ALT_POL(0x0020);
+const uint16_t    ALT_MOD(0x0010);
 
-class MCP980X
+//bit definitions for ambient temperature register MSB
+const uint8_t  ALT_CRIT(0x80);
+const uint8_t ALT_UPPER(0x40);
+const uint8_t ALT_LOWER(0x20);
+
+//I2C clock frequencies
+enum twiClockFreq_t { twiClock100kHz = 100000, twiClock400kHz = 400000 };
+
+class MCP9808
 {
     public:
-        MCP980X(uint8_t LS_ADDR_BITS);
-        void begin(void);
-        int readTempC16(MCP980X_REGS_t reg);
-        int readTempF10(MCP980X_REGS_t reg);
-        void writeTempC2(MCP980X_REGS_t reg, int value);
-        uint8_t readConfig(void);
-        void writeConfig(uint8_t value);
+        MCP9808(uint8_t LS_ADDR_BITS);
+        void
+            begin(twiClockFreq_t twiFreq = twiClock100kHz);
+
+        uint8_t
+            read(void),
+            write(void);
+        
+        int16_t
+            tUpper,         //upper temperature limit, in units of 1/4 °C
+            tLower,         //lower temperature limit, in units of 1/4 °C
+            tCritical,      //critical temperature limit, in units of 1/4 °C
+            tAmbient;       //ambient temperature, in units of 1/16 °C
+
+        uint16_t
+            config,
+            mfrID;
+            
+        uint8_t
+            deviceID,
+            deviceRev,
+            resolution;
+            
+        bool
+            alertCritical,
+            alertUpper,
+            alertLower;
 
     private:
-        uint8_t _devAddr;
+        uint8_t
+            _devAddr;
 };
 #endif
